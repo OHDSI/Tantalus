dee193576c2cf37900efd21b7929cbf6fc237678
Peter Rijnbeek <p.rijnbeek@erasmusmc.nl>
2018-06-11 23:28:47 +0200

    Update Shiny App
    Still needs a fix to allow loading the app after the Tantalus extraction and on a ShinyServer.

---
+++ inst/shinyApps/VocabCompareExplorer/man/readme.md
@@ -0,0 +1,57 @@
+Tantalus
+=========
+
+### Introduction
+
+Welcome to TANTALUS.
+
+TANTALUS is an open source application developed as a part of OHDSI intended to help expose differences between two vocabulary versions. 
+
+The data is generated using the [Tantalus R Package](https://github.com/OHDSI/Tantalus)
+
+The following comparison queries are currently available:
+
+<html>
+<table border="1">
+<thead>
+<tr class="header">
+<th>ID</th>
+<th>Description</th>
+</tr>
+</thead>
+<tbody>
+<tr class="odd">
+<td>9.01 </td>
+<td>Concept name changed</td>
+</tr>
+<tr class="even">
+<td>10.01 </td>
+<td>Concept domain changed</td>
+</tr>
+<tr class="odd">
+<td>99.01 </td>
+<td>Source concepts with new standard mappings, including 0</td>
+</tr>
+</tbody>
+</table>
+</html>
+
+
+In **tabular** view you can select the results for the following aggregation levels: description, domain, or vocabulary.
+
+After selecting an aggregation key, you can clicking on the rows in the table below to filter the results table. 
+Additional filtering and search options are available per column on the top of the table and overall on top of the page.
+
+**Graphical view** is under consideration.
+
+Note you can remove the side bar if more space is needed using the button in the header.
+
+### Getting Involved
+
+* Package manual: [Tantalus manual](https://raw.githubusercontent.com/OHDSI/Tantalus/master/extras/Tantalus.pdf) 
+* Developer questions/comments/feedback: <a href="http://forums.ohdsi.org/c/developers">OHDSI Forum</a>
+* We use the <a href="../../issues">GitHub issue tracker</a> for all bugs/issues/enhancements
+
+### Development status
+
+Under Development
--- inst/shinyApps/VocabCompareExplorer/ui.R
+++ inst/shinyApps/VocabCompareExplorer/ui.R
@@ -1,10 +1,73 @@
 library(shiny)
 library(DT)
+library(htmlwidgets)
+library(shinydashboard)
+library(markdown)
 
+ui <- dashboardPage(
+	dashboardHeader(title = "Tantalus",  titleWidth = 120),
+	dashboardSidebar(
+		width = 120,
+		sidebarMenu(
+			menuItem("Home", tabName = "home", icon = icon("home")),
+			menuItem("Tabular", tabName = "tabular", icon = icon("table")),
+			menuItem(
+				"Graphical",
+				tabName = "graphical",
+				icon = icon("bar-chart-o")
+			),
+			menuItem("Feedback", icon = icon("comment"), href = "https://github.com/mi-erasmusmc/Tantalus/issues")
+		),
+		tags$footer(
+			align = "right",
+			style = "
+			position:absolute;
+			bottom:0;
+			width:100%;
+			height:175px;
+			color: black;
+			padding: 10px;
+			text-align:center;
+			background-color: #eee;
+			z-index: 1000;",
+			HTML(
+				"<a href=\"https://www.apache.org/licenses/LICENSE-2.0\">Apache 2.0</a>
+				<div style=\"margin-bottom:10px;\">open source software</div>
+				<div>provided by</div>
+				<div><a href=\"http://www.ohdsi.org\"><img src=\"images/ohdsi_color.png\" height=42 width = 100></a></div>
+				<div><a href=\"http://www.ohdsi.org\">join the journey</a> </div>
+				<div>"
+			)
+			)
+		),
 
-shinyUI(fluidPage(titlePanel("OHDSI Vocabulary Comparator"),
-                  fluidRow(column(2,
-                                  checkboxInput("filterZeroPrevalence", "Remove issues with zero prevalence"),
-                                  h3("Summaries"),
-                                  selectInput("aggregateKey", label = "Aggregation key", choices = c("Domain", "Vocabulary", "Description")),
-                                  dataTableOutput("perAggregateKey")), column(10, dataTableOutput("allResultsTable")))))
+	dashboardBody(
+		tabItems(
+			tabItem(tabName = "home",
+							includeMarkdown(file.path("man", "readme.md"))),
+
+			tabItem(tabName = "tabular",
+							fluidRow(
+								column(
+									width = 2,
+									selectInput(
+										"aggregateKey",
+										label = "Aggregation key",
+										choices = c("Domain", "Vocabulary", "Description")
+									),
+									checkboxInput("filterZeroPrevalence", "Remove issues with zero prevalence"),
+									dataTableOutput("perAggregateKey", width = 150)
+									#,
+									#verbatimTextOutput('label')
+								),
+								column(
+									width = 10,
+									offset = 0,
+									DTOutput("allResultsTable")
+								)
+							)),
+
+		tabItem(tabName = "graphical", h3("Coming soon!"))
+
+	))
+)
